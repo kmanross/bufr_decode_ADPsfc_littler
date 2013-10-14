5cee7570ca561c850293aa1df5732ee3504a2965
Kevin L. Manross <manross@ucar.edu>
2013-10-14 14:14:26 -0600

    Updated install.sh to compile/link with the new BUFRLIB libraries

--- install/install.sh
+++ install/install.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 #  ------------------------------------------------------------------------
-#  This script will make bufrland.x and bufrship.x which extract data
-#  from ADP BUFR input files, and place the data into a basic text file.
-#  bufr_sfc2ob.x:  used to convert gdas.adpsfc.tHHz.YYYYMMDD.bufr files to obs format.
-#  bufr_ship2ob.x  used to convert gdas.sfcshp.tHHz.YYYYMMDD.bufr files  to obs format.
-#  runob2lit_imd_obs.x  used to convert/combine obs format files into one littlr format file.
+#  This script will make bufrupprair.x which to extract data from ADP BUFR
+#  input files, and place the data into a basic text file.  It is used to
+#  extract data from these kinds of files:
+#      gdas.adpupa.tHHz.YYYYMMDD.bufr 
+#      gdas.aircft.tHHz.YYYYMMDD.bufr
+#      gdas.satwnd.tHHz.YYYYMMDD.bufr 
+#      gdas.aircar.tHHz.YYYYMMDD.bufr
+#
 #  dumpbufr.x:        used to dump all contents of a BUFR file.
 #  ** Make sure the "ar" command location has been set in your path
 #  environment variable.  Type "which ar" to check if this is done. **
@@ -13,13 +16,14 @@
 set -eua
  
 #  ------------------------------------------------------------------------
-#  CPLAT - platform type (sgi,aix,sun)
+#  CPLAT - platform type (linux,sgi,aix,sun)
 #  ------------------------------------------------------------------------
  
 CPLAT=linux
 SRC=../src
 LIB=../lib
 EXE=../exe
+INSTALL=.
 
 #  different platforms use different link name protocols
 #  -----------------------------------------------------
@@ -33,77 +37,47 @@ fflag=""
 
 if [ $CPLAT = linux ]
 then
-   openrb=openrb_
-   openwb=openwb_
-   crdbfr=crdbufr_
-   cwrbfr=cwrbufr_
-   lenmsg=lenm_
-   cflag="-DUNDERSCORE"
-   fflag="-fno-second-underscore -fsloppy-char"
-   cc=gcc; ff=g95
-# uncomment following if ff=gfortran #
-#   fflag="-fno-second-underscore"
-#   cc=gcc; ff=gfortran
-elif [ $CPLAT = sgi ]
-then
-   openrb=openrb_
-   openwb=openwb_
-   crdbfr=crdbufr_
-   cwrbfr=cwrbufr_
-   lenmsg=lenm_
-   cflag=-DUNDERSCORE
-   cc=cc; ff=f77
-elif [ $CPLAT = aix ]
-then
-   openrb=openrb
-   openwb=openwb
-   crdbfr=crdbufr
-   cwrbfr=cwrbufr
-   lenmsg=lenm
-   cc=cc; ff=f77
-elif [ $CPLAT = sun ]
-then
-   openrb=openrb_
-   openwb=openwb_
-   crdbfr=crdbufr_
-   cwrbfr=cwrbufr_
-   lenmsg=lenm_
-   cflag=-DUNDERSCORE 
-   cc=cc; ff=f77
+   export FC=gfortran
+   export CC=gcc
+   fflag=" -O3 -DUNDERSCORE -fno-second-underscore -w"
+   cflag=" -O3 -DUNDERSCORE -w"
 fi
 
 #  Compile and archive the Bufr Library
 #  ------------------------------------
+echo "Compiling BUFRLIB Library..."
+cd $LIB
+if [ -e bufrlib.a ]
+then
+  rm bufrlib.a
+fi
+$LIB/makebufrlib.sh
+cd $INSTALL
 
-$cc $cflag -c $cflag $LIB/*.c
-$ff $fflag -c $LIB/*.f
-
-ar crv $LIB/bufrlib.a *.o
-
-rm *.o
- 
 #  Compile the decode programs
 #  ---------------------------------------
  
-$ff $fflag -c $SRC/dumpbufr.f
+echo "Compiling bufr_configdecode_ADPupa programs..."
+$FC $fflag -c $SRC/dumpbufr.f
+$FC $fflag -c $SRC/bufr_sfc2ob.f
+$FC $fflag -c $SRC/bufr_ship2ob.f
 
-$ff $fflag -c $SRC/bufr_sfc2ob.f
-$ff $fflag -c $SRC/bufr_ship2ob.f
+$FC $fflag -c $SRC/runob2lit_imd_obs.f
 
-$ff $fflag -c $SRC/runob2lit_imd_obs.f
  
 #  link and load the executables
 #  -----------------------------
 
+echo "Linking..."
+$FC $fflag -o $EXE/dumpbufr.x dumpbufr.o $LIB/bufrlib.a
 
-$ff $fflag -o $EXE/dumpbufr.x dumpbufr.o $LIB/bufrlib.a
+$FC $fflag -o $EXE/bufr_sfc2ob.x bufr_sfc2ob.o $LIB/bufrlib.a
+$FC $fflag -o $EXE/bufr_ship2ob.x bufr_ship2ob.o $LIB/bufrlib.a
 
-$ff $fflag -o $EXE/bufr_sfc2ob.x bufr_sfc2ob.o $LIB/bufrlib.a
-$ff $fflag -o $EXE/bufr_ship2ob.x bufr_ship2ob.o $LIB/bufrlib.a
-
-$ff $fflag -o $EXE/runob2lit_imd_obs.x runob2lit_imd_obs.o
-
+$FC $fflag -o $EXE/runob2lit_imd_obs.x runob2lit_imd_obs.o
 #  clean up
 #  --------
 
 rm -f *.o
+
+echo "Finished."
